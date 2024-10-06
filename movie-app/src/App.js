f247afeeb23fa8d1506f802d8bbd2ff0be225564
Liam Ryan <128687090+LiamRyan1@users.noreply.github.com>
2024-10-06 18:15:57 +0100

    Update App.js

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -7,10 +7,10 @@ import Content from './components/Content';
 
 function App() {
   return (
-    <Router>
+    <Router>{/*Enables url based nav*/}
       <NavigationBar />{/*calling navigationbar from components*/}
-      <Routes>
-        
+      <Routes>{/*manages different route definitions*/}
+        {/*specifies componets to render based on url */}
         <Route path="/home" element={<Content/>} />
         <Route path="/read" element={<h1> <Footer /></h1>} />
         <Route path="/create" element={<h1><Header /></h1>} />
